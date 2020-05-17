0151c72a9007b8fc4a8c38ea2865224039861521
Kuddleman <edl1155@gmail.com>
2020-05-16 20:10:53 -0700

    serialize form data

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm( e.target, { hash: true } )
+    console.log('Here are the values: ', values )
+  }
+
+  // if ( this.props.onCreateContact ) {
+  //   this.props.onCreateContact( values )
+  // }
 
   render() {
     return (
@@ -14,7 +26,7 @@ class CreateContact extends Component {
        >
          Close 
        </Link> 
-       <form className='create-contact-form'>
+       <form onSubmit={ this.handleSubmit } className='create-contact-form'>
        <ImageInput 
          className='create-contact-avatar-input' 
          name='avatarURL' 
